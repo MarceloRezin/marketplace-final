9349604552086e78097b9a289e5f59f1bd35d766
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-07-11 21:02:06 -0300

    Adição de um novo anúncio

---
+++ front/src/components/anuncios/AnuncioForm.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import { 
+  Slide, 
+  Dialog, 
+  AppBar, 
+  Toolbar,
+  Grid,
+  Typography,
+  IconButton, 
+  Button,
+  TextField
+} from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+import Select from '../select/Select';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default class AnuncioForm extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = this.getResetAnuncio();
+  }
+
+  getResetAnuncio = () => {
+    return {
+      id: null,
+      nome: "",
+      descricao: "",
+      valor: "",
+      usuario: -1,
+      categoria: -1,
+    };
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if(this.props.show && !prevProps.show){ //Foi aberto
+      if(this.props.anuncio == null){
+        this.setState(this.getResetAnuncio());
+      }else{
+        this.setState({
+          id: this.props.anuncio._id,
+          nome: this.props.anuncio.nome,
+          descricao: this.props.anuncio.descricao,
+          valor: this.props.anuncio.valor,
+          usuario: this.props.anuncio.usuario,
+          categoria: this.props.anuncio.categoria,
+        });
+      }
+    }
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.handleSubmit({
+      id: this.state.id,
+      nome: this.state.nome,
+      descricao: this.state.descricao,
+      valor: this.state.valor,
+      usuario: this.state.usuario,
+      categoria: this.state.categoria,
+    });
+  }
+
+  render(){
+    return(
+      <Dialog
+        fullScreen
+        open={this.props.show}
+        onClose={this.props.handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar style={{position: "relative"}}>
+          <Toolbar>
+            <Typography style={{flex: 1}} variant="h6">
+              {this.state.id === null ? "Novo" : "Editar"} Anúncio
+            </Typography>
+            <IconButton edge="end" color="inherit" onClick={this.props.handleClose}>
+              <Close />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <form onSubmit={this.handleSubmit}>
+          <Grid 
+            container 
+            direction="column"
+            alignItems="center"
+            spacing={2}
+            style={{"marginTop": 10}}
+          >
+            <Grid item>
+              <Grid 
+                container 
+                direction="row"
+                alignItems="center"
+                spacing={2}
+              >
+                <Grid item>
+                  <TextField type="text" id="nome" label="Nome" variant="outlined" value={this.state.nome} onChange={this.handleChange} required />
+                </Grid>
+                <Grid item>
+                  <TextField type="number" id="valor" label="Valor" variant="outlined" value={this.state.valor} onChange={this.handleChange} required />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Grid 
+                container 
+                direction="row"
+                alignItems="center"
+                spacing={2}
+              >
+                <Grid item>
+                  <Select id="categoria" label="Categoria" value={this.state.categoria} onChange={this.handleChange} itens={this.props.categorias} />
+                </Grid>
+                <Grid item>
+                  <Select id="usuario" label="Usuário" value={this.state.usuarios} onChange={this.handleChange} itens={this.props.usuarios} required />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <TextField
+                type="text"
+                id="descricao"
+                label="Descrição"
+                variant="outlined"
+                value={this.state.descricao}
+                onChange={this.handleChange}
+                style={{minWidth: 470}}  
+              />
+            </Grid>
+            <Grid item>
+              <Button color="primary" variant="outlined" type="submit">Salvar</Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Dialog>
+    );
+  }
+}
--- front/src/components/anuncios/Anuncios.js
+++ front/src/components/anuncios/Anuncios.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 import Toast from '../toast/Toast';
 import AnunciosList from './AnunciosList';
+import AnuncioForm from './AnuncioForm';
 import { Grid, TextField, IconButton } from '@material-ui/core';
 import { Search, NoteAdd } from '@material-ui/icons';
 
@@ -14,6 +15,12 @@ export default class Anuncios extends React.Component{
       query: "",
       anuncios: [],
 
+      anuncioForm: null,
+      showForm: false,
+
+      categorias: [],
+      usuarios: [],
+
       toastTipo: "",
       toastOpen: false,
       toastMensagem: ""
@@ -22,6 +29,8 @@ export default class Anuncios extends React.Component{
 
   componentDidMount = () => {
     this.handleList();
+    this.handleListCategorias();
+    this.handleListUsuarios();
   }
 
   handleChange = (event) => {
@@ -38,17 +47,83 @@ export default class Anuncios extends React.Component{
       });
     });
   }
+  
+  handleListCategorias = () => {
+    Axios.get("http://localhost:3001/categorias").then((resp) => {
+      this.setState({
+        categorias: resp.data
+      });
+    });
+  }
+  
+  handleListUsuarios = () => {
+    Axios.get("http://localhost:3001/usuarios").then((resp) => {
+      this.setState({
+        usuarios: resp.data
+      });
+    });
+  }
 
-  handlePost = (usuario) => {   
-    Axios.post("http://localhost:3001/usuarios", usuario).then((resp) => {
+  handleSave = (anuncio) => {
+    
+    console.log(anuncio.usuario);
+    
+    if(anuncio.usuario < 0){
+      this.showToast("Informe o usuário.", "error");
+      return;
+    }else{
+      anuncio.usuario = this.state.usuarios[anuncio.usuario];
+    }
 
-      this.showToast("Cadastro efetuado com sucesso.", "success");
-      setTimeout(() => this.props.history.push("/login"), 2000);
+    if(anuncio.categoria < 0){ //Não é obrigatório
+      delete anuncio.categoria
+    }else{
+      anuncio.categoria = this.state.categorias[anuncio.categoria];
+    }
+
+    if(anuncio.id == null){ //Novo
+      this.handlePost(anuncio);
+    }else{ //Edição
+      this.handlePut(anuncio);
+    }
+  }
+
+  handlePost = (anuncio) => {   
+    Axios.post("http://localhost:3001/anuncios", anuncio).then((resp) => {
+
+      this.handleList();
+      this.closeForm();
+      this.showToast("Anúncio registrado com sucesso.", "success");
 
     }).catch((err) => {
       this.showToast(err.response.data.error, "error");
     });
+  };
+  
+  handlePut = (anuncio) => {   
+    Axios.put("http://localhost:3001/anuncios" + anuncio.id, anuncio).then((resp) => {
+
+      this.handleList();
+      this.closeForm();
+      this.showToast("Anúncio atualizado com sucesso.", "success");
+
+    }).catch((err) => {
+      this.showToast(err.response.data.error, "error");
+    });
+  };
+
+  openForm = (anuncioForm) => {
+    this.setState({
+      showForm: true,
+      anuncioForm: anuncioForm
+    });
   }
+
+  closeForm = () => {
+    this.setState({
+      showForm: false
+    });
+  };
 
   showToast = (mensagem, tipo) => {
     this.setState({
@@ -93,7 +168,7 @@ export default class Anuncios extends React.Component{
               </IconButton>
             </Grid>
             <Grid item>
-              <IconButton aria-label="Novo anúncio" component="span">
+              <IconButton aria-label="Novo anúncio" component="span" onClick={() => this.openForm(null)}>
                 <NoteAdd style={{ color: 'black' }} />
               </IconButton>
             </Grid>
@@ -102,6 +177,7 @@ export default class Anuncios extends React.Component{
         <Grid item>
           <AnunciosList handleList={this.handleList} anuncios={this.state.anuncios} />
         </Grid>
+        <AnuncioForm show={this.state.showForm} anuncio={this.state.anuncioForm} categorias={this.state.categorias.map( c => c.nome)} usuarios={this.state.usuarios.map( c => c.nome)} handleClose={this.closeForm} handleSubmit={this.handleSave} />
         <Toast open={this.state.toastOpen} type={this.state.toastTipo} message={this.state.toastMensagem} handleClose={this.closeToast} />
     </Grid>
     );
